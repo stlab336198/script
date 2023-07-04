2ad37e672b4cd1a80d6683082b335abf64610c3b
stlab336198 <138571717+stlab336198@users.noreply.github.com>
2023-07-04 17:00:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,21 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from webdriver_manager.chrome import ChromeDriverManager
+import time
 
+driver=webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+assert "Swag Labs" in driver.title
 
-
-
-URL = 'https://www.saucedemo.com/'
-LOGIN = 'standard_user'
-PASSWORD = 'secret_sauce'
-
-def get_driver():
-    chrome_options = Options()
-    chrome_options.add_argument("--window-size=1920,800")
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
-                              options=chrome_options)
-    driver.implicitly_wait(10)
-    return driver
-
-
-def open_page(driver, url):
-    driver.get(url)
-
-def get_element_by_id(driver, locator):
-    return driver.find_element(By.ID, locator)
-
-def element_click(driver, locator):
-    element = get_element_by_id(driver, locator)
-    element.click()
-
-def element_send_keys(driver, locator, text):
-    element = get_element_by_id(driver, locator)
-    element.send_keys(text)
-
-def login(driver, name, password):
-    element_send_keys(driver, 'user-name', name)
-    element_send_keys(driver, 'password', password)
-    element_click(driver, 'login-button')
-
-
-
-driver = get_driver()
-open_page(driver, URL)
-login(driver=driver, name=LOGIN, password=PASSWORD)
-
-driver.quit()
+elem=driver.find_element(By.ID,"user-name")
+elem.clear()
+elem.send_keys("standard_user")
+elem.send_keys(Keys.RETURN)
+passwd=driver.find_element(By.ID,"password")
+passwd.clear()
+passwd.send_keys("secret_sauce")
+passwd.send_keys(Keys.RETURN)
+button=driver.find_element(By.ID,"login-button")
+button.click()
+time.sleep(5)
+driver.close()
